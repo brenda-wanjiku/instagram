118709b8e824761b141d67e86594de5222c91c98
brenda-wanjiku <brendawanjiku@live.com>
2020-05-29 16:38:04 +0300

    Creates Image and Profile model

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.CharField(max_length=30)
+    img_name = models.CharField(max_length=60)
+    caption = models.CharField(max_length=100)
+    likes = models.CharField(max_length=30)
+    comments = models.CharField(max_length=30)
+
+
+class Profile(models.Model):
+    profile_photo = models.CharField(max_length=30)
+    bio = models.CharField(max_length=30)

4991db989714e947ab820d7c75328698b31b9b85
brenda-wanjiku <brendawanjiku@live.com>
2020-05-29 22:52:08 +0300

    Configures media setting. Displays all image data on homepage.

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='insta/')
     img_name = models.CharField(max_length=60)
     caption = models.CharField(max_length=100)
     likes = models.CharField(max_length=30)
@@ -16,6 +16,13 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+
+    @classmethod
+    def update_caption(cls,id,caption):
+        cls.objects.filter(id=id).update(caption=caption)
+        updated_caption = cls.objects.get(id=id)
+        return updated_image
+
 
 class Profile(models.Model):
     profile_photo = models.CharField(max_length=30)
--- instagram/insta/views.py
+++ instagram/insta/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'homepage.html')
+    images = Image.objects.all()
+    return render(request, 'homepage.html', {'images': images})
